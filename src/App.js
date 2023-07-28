78c6de68a2d6edaf1f79324925f22a3aa6bc57df
maria de los angeles <mariaramireeez2002@gmail.com>
2023-07-28 18:42:51 -0400

    fix: add format glb

--- src/App.js
+++ src/App.js
@@ -1,90 +1,93 @@
-import './App.css';
-import * as THREE from 'three';
-import FBXLoader from 'three-fbx-loader';
+import "./App.css";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-import {useRef, useState,useEffect} from "react";
+import { useRef, useState, useEffect } from "react";
 function App() {
-    const containerRef = useRef(null);
-    const [animationMixer, setAnimationMixer] = useState(null);
+  const containerRef = useRef(null);
 
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
 
-    useEffect (() => {
-      const scene = new THREE.Scene();
-      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    containerRef.current.appendChild(renderer.domElement);
+    //const geometry = new THREE.BoxGeometry(1, 1, 1);
+    //const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    // const cube = new THREE.Mesh(geometry, material);
+    const loader = new GLTFLoader();
+    loader.load(`${process.env.PUBLIC_URL}/menu.glb`, (glb) => {
+      const root = glb.scene;
+      root.scale.set(0.5, 0.5, 0.5);
+      scene.add(root);
+    });
+    const light = new THREE.DirectionalLight();
+    light.position.set(2, 2, 5);
+    scene.add(light);
+    //scene.add(cube);
 
-      const renderer = new THREE.WebGLRenderer();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      containerRef.current.appendChild(renderer.domElement);
-      const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-      const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-      const cube = new THREE.Mesh( geometry, material );
-      scene.add( cube );
+    camera.position.z = 5;
+    function animate() {
+      requestAnimationFrame(animate);
+      // cube.rotation.x += 0.01;
+      //cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    }
+    animate();
+  });
 
-      camera.position.z = 5;
-      function animate() {
-        requestAnimationFrame( animate );
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-        renderer.render( scene, camera );
+  // useEffect(() => {
+  //     // Configurar la escena, cámara y renderizador
+  //     const scene = new THREE.Scene();
+  //     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  //     camera.position.z = 5;
+  //     const renderer = new THREE.WebGLRenderer();
+  //     renderer.setSize(window.innerWidth, window.innerHeight);
+  //     containerRef.current.appendChild(renderer.domElement);
 
-      }
-      animate();
-    
-    
-    
-    }, );
+  //     // Cargar el archivo FBX y obtener la animación
+  //     const loader = new FBXLoader();
+  //     loader.load(`${process.env.PUBLIC_URL}/menu.glb`, (model) => {
+  //         scene.add(model);
 
+  //         // Obtener la animación del modelo
+  //         const mixer = new THREE.AnimationMixer(model);
+  //         const action = mixer.clipAction(model.animations[0]); // Puedes cambiar el índice si hay varias animaciones
 
+  //         // Reproducir la animación
+  //         action.play();
+  //         setAnimationMixer(mixer);
+  //     });
 
-    // useEffect(() => {
-    //     // Configurar la escena, cámara y renderizador
-    //     const scene = new THREE.Scene();
-    //     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    //     camera.position.z = 5;
-    //     const renderer = new THREE.WebGLRenderer();
-    //     renderer.setSize(window.innerWidth, window.innerHeight);
-    //     containerRef.current.appendChild(renderer.domElement);
+  //     // Función para actualizar la animación en cada cuadro
+  //     const animate = () => {
+  //         requestAnimationFrame(animate);
 
-    //     // Cargar el archivo FBX y obtener la animación
-    //     const loader = new FBXLoader();
-    //     loader.load(`${process.env.PUBLIC_URL}/menu.glb`, (model) => {
-    //         scene.add(model);
+  //         // Actualizar la animación
+  //         if (animationMixer) {
+  //             animationMixer.update(0.016); // El tiempo delta puede variar según tu aplicación
+  //         }
 
-    //         // Obtener la animación del modelo
-    //         const mixer = new THREE.AnimationMixer(model);
-    //         const action = mixer.clipAction(model.animations[0]); // Puedes cambiar el índice si hay varias animaciones
+  //         // Renderizar la escena
+  //         renderer.render(scene, camera);
+  //     };
 
-    //         // Reproducir la animación
-    //         action.play();
-    //         setAnimationMixer(mixer);
-    //     });
+  //     // Iniciar la animación
+  //     animate();
 
-    //     // Función para actualizar la animación en cada cuadro
-    //     const animate = () => {
-    //         requestAnimationFrame(animate);
-
-    //         // Actualizar la animación
-    //         if (animationMixer) {
-    //             animationMixer.update(0.016); // El tiempo delta puede variar según tu aplicación
-    //         }
-
-    //         // Renderizar la escena
-    //         renderer.render(scene, camera);
-    //     };
-
-    //     // Iniciar la animación
-    //     animate();
-
-    //     // Limpiar la escena cuando el componente se desmonta
-    //     return () => {
-    //         scene.remove();
-    //         renderer.dispose();
-    //     };
-    // }, [animationMixer]);
-  return (
-      <div ref={containerRef} />
-
-  );
+  //     // Limpiar la escena cuando el componente se desmonta
+  //     return () => {
+  //         scene.remove();
+  //         renderer.dispose();
+  //     };
+  // }, [animationMixer]);
+  return <div ref={containerRef} />;
 }
 
 export default App;
