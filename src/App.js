8c094655083ae31fcd63019c01e041deac55c2f1
maria de los angeles <mariaramireeez2002@gmail.com>
2023-07-28 19:44:09 -0400

    set time out

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 import { useRef, useState, useEffect } from "react";
+import { MeshStandardMaterial } from "three";
 function App() {
   const containerRef = useRef(null);
+
+  const rootRef = useRef(null); // Ref para acceder al root
 
   useEffect(() => {
     const scene = new THREE.Scene();
@@ -24,7 +27,20 @@ function App() {
     const loader = new GLTFLoader();
     loader.load(`${process.env.PUBLIC_URL}/menu.glb`, (glb) => {
       const root = glb.scene;
-      root.scale.set(0.2, 0.09, 0.3);
+      root.scale.set(0.2, 0.07, 0.4);
+      root.traverse((child) => {
+        if (child.isMesh) {
+          // Crear un nuevo material de color verde
+          const material = new THREE.MeshStandardMaterial({
+            color: 0x00ff00,
+          });
+
+          // Aplicar el nuevo material a la malla
+          child.material = material;
+        }
+      });
+      rootRef.current = root;
+
       scene.add(root);
     });
     const light = new THREE.DirectionalLight();
@@ -33,11 +49,20 @@ function App() {
     //scene.add(cube);
 
     camera.position.z = 3;
+    camera.position.x = -1;
     function animate() {
-      requestAnimationFrame(animate);
-      // cube.rotation.x += 0.01;
-      //cube.rotation.y += 0.01;
-      renderer.render(scene, camera);
+      setTimeout(() => {
+        requestAnimationFrame(animate);
+        // cube.rotation.x += 0.01;
+        //cube.rotation.y += 0.01;
+        // Acceder al root y aplicar rotación gradual en cada frame
+        if (rootRef.current) {
+          console.log("frame");
+          rootRef.current.rotation.y += 0.01;
+          rootRef.current.rotation.x += 0.02;
+        }
+        renderer.render(scene, camera);
+      }, 600);
     }
     animate();
   });
