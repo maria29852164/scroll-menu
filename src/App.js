1a67ddb7b408f42db813673227516f515a81f935
maria de los angeles <mariaramireeez2002@gmail.com>
2023-07-28 19:04:49 -0400

    feat: gusano app

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
     const loader = new GLTFLoader();
     loader.load(`${process.env.PUBLIC_URL}/menu.glb`, (glb) => {
       const root = glb.scene;
-      root.scale.set(0.5, 0.5, 0.5);
+      root.scale.set(0.2, 0.09, 0.3);
       scene.add(root);
     });
     const light = new THREE.DirectionalLight();
@@ -32,7 +32,7 @@ function App() {
     scene.add(light);
     //scene.add(cube);
 
-    camera.position.z = 5;
+    camera.position.z = 3;
     function animate() {
       requestAnimationFrame(animate);
       // cube.rotation.x += 0.01;
@@ -42,51 +42,6 @@ function App() {
     animate();
   });
 
-  // useEffect(() => {
-  //     // Configurar la escena, cámara y renderizador
-  //     const scene = new THREE.Scene();
-  //     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  //     camera.position.z = 5;
-  //     const renderer = new THREE.WebGLRenderer();
-  //     renderer.setSize(window.innerWidth, window.innerHeight);
-  //     containerRef.current.appendChild(renderer.domElement);
-
-  //     // Cargar el archivo FBX y obtener la animación
-  //     const loader = new FBXLoader();
-  //     loader.load(`${process.env.PUBLIC_URL}/menu.glb`, (model) => {
-  //         scene.add(model);
-
-  //         // Obtener la animación del modelo
-  //         const mixer = new THREE.AnimationMixer(model);
-  //         const action = mixer.clipAction(model.animations[0]); // Puedes cambiar el índice si hay varias animaciones
-
-  //         // Reproducir la animación
-  //         action.play();
-  //         setAnimationMixer(mixer);
-  //     });
-
-  //     // Función para actualizar la animación en cada cuadro
-  //     const animate = () => {
-  //         requestAnimationFrame(animate);
-
-  //         // Actualizar la animación
-  //         if (animationMixer) {
-  //             animationMixer.update(0.016); // El tiempo delta puede variar según tu aplicación
-  //         }
-
-  //         // Renderizar la escena
-  //         renderer.render(scene, camera);
-  //     };
-
-  //     // Iniciar la animación
-  //     animate();
-
-  //     // Limpiar la escena cuando el componente se desmonta
-  //     return () => {
-  //         scene.remove();
-  //         renderer.dispose();
-  //     };
-  // }, [animationMixer]);
   return <div ref={containerRef} />;
 }
 
