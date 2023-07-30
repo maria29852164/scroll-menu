9719362eb3002255a2944231f1b410964b75fd82
maria de los angeles <mariaramireeez2002@gmail.com>
2023-07-30 12:54:33 -0400

    fix: changes

--- src/App.js
+++ src/App.js
@@ -23,16 +23,28 @@ function App() {
     const loader = new GLTFLoader();
     loader.load(`${process.env.PUBLIC_URL}/menu.glb`, (glb) => {
       const model = glb.scene;
-      model.scale.set(0.1, 0.07, 0.4);
+      model.scale.set(0.2, 0.07, 0.6);
 
       // Crear un nuevo material de color verde
-      const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+      const material = new THREE.MeshStandardMaterial();
 
       // Recorrer todas las mallas del modelo y aplicar el nuevo material
       model.traverse((child) => {
-        if (child.isMesh) {
-          child.material = material;
-        }
+        model.traverse((child) => {
+          if (child.isMesh) {
+            const material = child.material;
+            console.log(material);
+            if (material.map) {
+              // Si existe una textura en el material, obtener la textura
+              const texture = material.map;
+              console.log("Textura encontrada:", texture);
+
+              // Ahora puedes utilizar la 'texture' en Three.js, por ejemplo:
+              // const materialConTextura = new THREE.MeshBasicMaterial({ map: texture });
+              // child.material = materialConTextura;
+            }
+          }
+        });
       });
 
       scene.add(model);
@@ -62,7 +74,7 @@ function App() {
 
       // Verificar si el modelo está disponible antes de aplicar la rotación
       if (modelRef.current) {
-        modelRef.current.rotation.x += 0.01; // Ajusta la velocidad de rotación aquí
+        //   modelRef.current.rotation.x += 0.01; // Ajusta la velocidad de rotación aquí
       }
 
       renderer.render(scene, camera);
